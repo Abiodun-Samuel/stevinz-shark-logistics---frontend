93a53dce8db0880ea4571e3a00454044c7cf5256
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2025-01-11 05:40:00 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <div class="img">
+    <img
+      src="https://img.freepik.com/free-vector/construction-poster_1284-5166.jpg?semt=ais_hybrid"
+      alt=""
+    />
+  </div>
+</template>
+
+<style scoped>
+.img {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+}
+</style>
